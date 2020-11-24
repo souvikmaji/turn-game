28ef91749cc7d9a0f7d2854e4493b49f0b70c3f4
souvikmaji <souvikmaji94@gmail.com>
2020-11-25 00:00:25 +0530

    simple http server serving html page added

--- main.go
+++ main.go
@@ -3,10 +3,30 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"net/http"
 )
+
+var addr = flag.String("addr", ":8080", "http service address")
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	log.Println("serving home")
+	http.ServeFile(w, r, "home.html")
+}
+
+func serveWebsocket(w http.ResponseWriter, r *http.Request) {
+	log.Println("serving websocket")
+	fmt.Fprintf(w, "websocket")
+}
 
 func main() {
 	flag.Parse()
 
-	fmt.Println("hello world!")
+	http.HandleFunc("/", serveHome)
+	http.HandleFunc("/ws", serveWebsocket)
+
+	err := http.ListenAndServe(*addr, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }

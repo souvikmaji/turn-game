a435e7a8b75289c97473bb9a5238c4245314378f
souvikmaji <souvikmaji94@gmail.com>
2020-11-25 00:34:57 +0530

    using gorilla websocket library for managing socket connections

--- main.go
+++ main.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"flag"
-	"fmt"
+	"github.com/gorilla/websocket"
 	"log"
 	"net/http"
 )
 
 var addr = flag.String("addr", ":8080", "http service address")
+
+// use default options
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool { return true }, // for external ws testers
+}
 
 func serveHome(w http.ResponseWriter, r *http.Request) {
 	log.Println("serving home")
@@ -16,11 +21,31 @@ func serveHome(w http.ResponseWriter, r *http.Request) {
 
 func serveWebsocket(w http.ResponseWriter, r *http.Request) {
 	log.Println("serving websocket")
-	fmt.Fprintf(w, "websocket")
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrade:", err)
+		return
+	}
+	defer c.Close()
+	for {
+		mt, message, err := c.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			break
+		}
+		log.Printf("recv: %s", message)
+		err = c.WriteMessage(mt, message)
+		if err != nil {
+			log.Println("write:", err)
+			break
+		}
+	}
 }
 
 func main() {
 	flag.Parse()
+
+	log.Println("starting server at: ", *addr)
 
 	http.HandleFunc("/", serveHome)
 	http.HandleFunc("/ws", serveWebsocket)
